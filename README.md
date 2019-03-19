e1cdfa5f89cf3731ddf394a1df9ab74d0ffbe123
Lucas Fernandes <lucasfernandesforever@gmail.com>
2019-03-18 23:04:18 -0300

    🎉 Primeiro commit!

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# flight-cms
+# Flight CMS 🖥️🎨
+Transforme seu website estático em páginas poderosas com ferramentas incríveis para desenvolvedores e intuitivas para clientes. _Eesse projeto ainda está SUPER no começo. O texto abaixo se refere às características que guiarão o desenvolvimento._
+
+## Leve e rápido ⚡
+A ideia do **Flight CMS** é ter tudo o que você precisa para que seus clientes possam editar o conteúdo de seus websites de forma fácil (mas nada além disso), sem você ter que gastar horas desenvolvendo painéis personalizados.
+
+## Pronto pra viagem ✈️
+Configurar plataformas como Google Ads, Analytics e o próprio SEO do site são tarefas repetitivas que todo site precisa. Pare de gastar seu tempo fazendo tudo isso na mão e apenas preencha uns campos aqui e alí e _**voila**_!
+
+## Fácil de traduzir 💬
+Use tabelas em **.CSV** para levar seu website para o mundo todo! Localize seus textos para qualquer linguagem, usando seu editor de tabelas de preferência e, simples assim, seu site pode ser traduzido.
---
+++ hk/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Flight CMS - Housekeeping</title>
+  <link rel="stylesheet" href="../flight/assets/css/bulma.min.css">
+  <link rel="stylesheet" href="../flight/assets/css/hk.css">
+  <script defer src="../flight/assets/js/font-awesome-5.3.1.js"></script>
+</head>
+
+<body>
+
+<nav class="navbar">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="#"><h1 class="subtitle">Flight CMS</h1></a>
+    <a role="button" class="navbar-burger burger" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+
+    <div class="navbar-end">
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link"><figure class="image is-16x16" style="margin-right:.5em;"> <img src="https://pt.gravatar.com/userimage/123689047/7eb303aa04802143c23ac56a0602e8b0.jpeg" style="border-radius:50%;"> </figure> Lucas Fernandes</a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item"><span class="icon"><i class="fas fa-user"></i></span> <span>Meu perfil</span></a>
+          <a class="navbar-item"><span class="icon"><i class="fas fa-gavel"></i></span> <span>Registros</span></a>
+          <hr class="navbar-divider">
+          <a class="navbar-item"><span class="icon"><i class="fas fa-sign-out-alt"></i></span> <span>Desconectar</span></a>
+        </div>
+      </div>
+
+      <a class="navbar-item"><i class="fas fa-cog"></i></a>
+      <a class="navbar-item"><i class="far fa-life-ring"></i></a>
+      <a class="navbar-item"><i class="fas fa-power-off"></i></a>
+    </div>
+
+  </div>
+</nav>
+
+<div class="columns">
+
+<div class="column sidebar is-one-fifth">
+  <aside class="menu">
+    <p class="menu-label">Principal</p>
+    <ul class="menu-list">
+      <li><a class="is-active"><span class="icon"><i class="fas fa-tachometer-alt"></i></span> <span>Painel</span></a></li>
+      <li><a><span class="icon"><i class="fas fa-chart-pie"></i></span> <span>Estatísticas</span></a></li>
+    </ul>
+    <p class="menu-label">
+      Conteúdo
+    </p>
+    <ul class="menu-list">
+      <li><a><span class="icon"><i class="fas fa-file"></i></span> <span>Páginas</span></a></li>
+      <li><a><span class="icon"><i class="fas fa-newspaper"></i></span> <span>Publicações</span></a></li>
+      <li><a><span class="icon"><i class="fas fa-images"></i></span> <span>Galeria</span></a></li>
+      <li><a><span class="icon"><i class="fas fa-cubes"></i></span> <span>Widgets</span></a></li>
+    </ul>
+    <p class="menu-label">
+      Personalização
+    </p>
+    <ul class="menu-list">
+      <li><a><span class="icon"><i class="fas fa-palette"></i></span> <span>Estilos</span></a></li>
+      <li><a><span class="icon"><i class="fas fa-puzzle-piece"></i></span> <span>Extensões</span></a></li>
+      <li><a><span class="icon"><i class="fas fa-file"></i></span> <span>Opções</span></a></li>
+    </ul>
+    <p class="menu-label">
+      Administração
+    </p>
+    <ul class="menu-list">
+      <li><a><span class="icon"><i class="fas fa-toggle-on"></i></span> <span>Opções</span></a></li>
+      <li><a><span class="icon"><i class="fas fa-users"></i></span> <span>Usuários</span></a></li>
+    </ul>
+  </aside>
+</div>
+
+<div class="column">
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Hello world!</h1>
+      <p class="subtitle">Programmed to work and not to feel... <i class="fas fa-music"></i></p>
+      <iframe width="300" height="150" src="https://www.youtube.com/embed/Yw6u6YkTgQ4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </section>
+</div>
+
+</div>
+
+</body>
+
+</html>
